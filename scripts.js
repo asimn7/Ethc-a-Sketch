52d45ec575af279f4970073d9fffc3d737b2cddd
asimn7 <nepalasim924@gmail.com>
2024-03-08 12:04:02 +0545

    basic functionality made

---
+++ scripts.js
@@ -0,0 +1,35 @@
+let color='hsl($(Math.random()*360),100%,50%)'
+function boardSize(size){
+    let board=document.querySelector('.board');
+    let squares=document.querySelectorAll('.board .square');
+    squares.forEach((div)=>div.remove());
+board.style.gridTemplateColumns=`repeat(${size}, 1fr)`;
+board.style.gridTemplateRows=`repeat(${size}, 1fr)`;
+
+let amount=size*size;
+for(let i=0;i<amount;i++){ 
+let square=document.createElement('div'); //to create n div elements
+square.addEventListener('mouseover',colorSquare);
+square.style.background='white';
+board.insertAdjacentElement("beforeend", square);
+}
+}
+// 'hsl($(Math.random()*360),100%,50%)'
+boardSize(16);
+
+function changeSize(input){
+    if(input>1 && input<100){
+        boardSize(input);
+    }
+    else{
+        console.log("error, too many squares");
+    }
+}
+
+function colorSquare(){
+    this.style.background=color;
+}
+
+function changeColor(choice){
+    color=choice;
+}
