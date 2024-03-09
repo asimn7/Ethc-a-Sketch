0fd3dc4cef0eea17e19224162ceaf8769fe47fec
asimn7 <nepalasim924@gmail.com>
2024-03-09 22:58:49 +0545

    except slider everything done

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-let color='black'
+let color='black';
+let click=true;
 function boardSize(size){
     let board=document.querySelector('.board');
     let squares=document.querySelectorAll('.board div');
@@ -18,22 +19,27 @@ board.insertAdjacentElement("beforeend", square);
 boardSize(16);
 
 function changeSize(input){
-    if(input>1 && input<100){
-        boardSize(input);
-    }
-    else{
-        console.log("error, too many squares");
+    if (click){
+        if(input>1 && input<100){
+            boardSize(input);
+        }
+        else{
+            console.log("error, too many squares");
+        }
     }
 }
 
 function colorSquare(){
-    if(color==='random'){
+    if(click){
+        if(color==='random'){
        this.style.backgroundColor=`hsl(${Math.random() * 360}, 100%, 50%)`;
     }
     else{
         this.style.backgroundColor=color;
     }
 }
+}
+
 function changeColor(choice){
     color=choice;
 }
@@ -43,3 +49,20 @@ function resetSketchpad(){
     let squares=document.querySelectorAll('.board div');
     squares.forEach((div)=>div.style.backgroundColor='white');
     }
+
+
+document.querySelector("body").addEventListener("click",(e)=>{
+    const mode=document.querySelector(".mode");
+if(e.target.tagName != 'BUTTON'){
+    click = !click;
+if(click){
+    mode.textContent="Mode: Coloring";
+
+}
+else{
+    mode.textContent="Mode: Not Coloring"
+}
+mode.style.fontSize="20px";
+mode.style.fontStyle="italic";
+}
+});
